70a826d89e072e2da6b0448e5c5c05c46eceeca7
Devilboi99 <Turnickii.n@gmail.com>
2022-04-18 09:48:20 +0500

    createBase

--- FutureGame/Form1.cs
+++ FutureGame/Form1.cs
@@ -12,9 +12,47 @@ namespace FutureGame
 {
     public partial class Form1 : Form
     {
+        private Player _player = new Player(0,0);
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var graphics = e.Graphics;
+            var brush = new SolidBrush(Color.Blue);
+            graphics.FillRectangle(brush,_player.x,_player.y,30,30);
+        }
+
+        
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            
+            switch (e.KeyChar)
+            {
+                case 'w':
+                    Player.Move(Directrion.Up);
+                    break;
+                case 'd':
+                    Player.Move(Directrion.Right);
+                    break;
+                case 'a':
+                    Player.Move(Directrion.Left);
+                    break;
+                case 's':
+                    Player.Move(Directrion.Down);
+                    break;
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            var move = new creat
+            switch (Keys)
+            {
+                
+            }
         }
     }
 }
---
+++ FutureGame/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public enum Directrion
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public class Player
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+
+        public Player(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static void  Move(Directrion dir)
+        {
+            Func<(int, int)> move;
+            var command = new Dictionary<Directrion, Func<(int, int)>>();
+            command.Add(Directrion.Down, () => );
+            
+        }
+    }
